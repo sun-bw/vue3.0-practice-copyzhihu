86ebe7f4bafe4cd74004ba58210ace2317a271dc
sun <857153966@qq.com>
2021-01-23 15:01:42 +0800

    header组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="currentUser"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,11 @@
 import { defineComponent } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GloballHeader.vue'
+const currentUser: UserProps = {
+  isLogin: true,
+  name: '温言'
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -25,11 +31,13 @@ const testData: ColumnProps[] = [
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup() {
     return {
-      list: testData
+      list: testData,
+      currentUser
     }
   }
 });
