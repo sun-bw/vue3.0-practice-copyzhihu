2336e9e374155deefbe1d3aa1215e4f1efec6705
sun <857153966@qq.com>
2021-01-23 15:47:33 +0800

    feat：down，点击外部区域关闭逻辑

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="dropdown">
+<div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outlight my-2 dropdown-toggle" @click.prevent="toggleOpen">
         {{title}}
     </a>
@@ -9,7 +9,7 @@
 </div>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from "vue"
+import { defineComponent, ref, onMounted, onUnmounted } from "vue"
 export default defineComponent({
     name: 'Dropdown',
     props: {
@@ -20,12 +20,28 @@ export default defineComponent({
     },
     setup() {
         const isOpen = ref(false);
+        const dropdownRef = ref<null | HTMLElement>(null);
         const toggleOpen = () => {
             isOpen.value = !isOpen.value
         };
+        const handler = (e: MouseEvent) => {
+            if(dropdownRef.value) {
+                // 是否包含当前节点 contains
+                if(!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value === true) {
+                    isOpen.value = false
+                }
+            }
+        }
+        onMounted(() => {
+            document.addEventListener('click', handler)
+        })
+        onUnmounted(() => {
+            document.addEventListener('click', handler)
+        })
         return {
             isOpen,
-            toggleOpen
+            toggleOpen,
+            dropdownRef
         }
     }
 })
