eb79a09b1f006609c535b28704243bda3aa01c62
sun <857153966@qq.com>
2021-01-24 15:01:13 +0800

    feat：表单验证

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,75 @@
 <template>
   <div class="container">
     <global-header :user="currentUser"></global-header>
-    <column-list :list="list"></column-list>
+    <!-- <column-list :list="list"></column-list> -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInoutEmail1" class="form-label">邮箱地址</label>
+        <input 
+          type="email"  class="form-control" id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{emailRef.message}}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword">
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+// import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GloballHeader.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: '温言'
 }
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是test1的专刊',
-    avatat: '',
-  },
-  {
-    id: 2,
-    title: 'test1的专栏',
-    description: '这是test1的专刊',
-    avatat: '',
-  }
-]
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是test1的专刊',
+//     avatat: '',
+//   },
+//   {
+//     id: 2,
+//     title: 'test1的专栏',
+//     description: '这是test1的专刊',
+//     avatat: '',
+//   }
+// ]
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
+    // ColumnList,
     GlobalHeader
   },
   setup() {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: '',
+    })
+    const validateEmail = () => {
+      if(emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if(!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
-      list: testData,
-      currentUser
+      // list: testData,
+      currentUser,
+      emailRef,
+      validateEmail
     }
   }
 });
